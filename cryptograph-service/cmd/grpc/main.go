9929d44e1bfcc31db7a6f8fd4455d684b310ed6b
hsuliz <hlib.suliz@gmail.com>
2025-03-02 08:48:17 +0100

    refactor

--- cryptograph-service/cmd/grpc/main.go
+++ cryptograph-service/cmd/grpc/main.go
@@ -3,26 +3,19 @@ package main
 import (
 	"cryptograph-service/cmd/grpc/server"
 	"cryptograph-service/internal/pb"
-	"flag"
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
-var (
-	port = flag.Int("port", 8090, "The server port")
-)
-
 func main() {
-	flag.Parse()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 8080))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	log.Printf("server listening at %v", lis.Addr())
 
+	log.Printf("server listening at %v", lis.Addr())
 	grpcServer := grpc.NewServer()
 	pb.RegisterCryptographServiceServer(grpcServer, &server.Server{})
 	if err := grpcServer.Serve(lis); err != nil {
